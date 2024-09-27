432b15fcfa94fbda674baeb7b0b35b1f4b4956e6
kijiwon <kiji1313@gmail.com>
2024-09-27 11:25:14 +0900

    book page static params

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,4 +1,10 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+// 정적인 파라미터를 생성하는 함수 <- 빌드 타임에 정적 파라미터를 읽어 헤당 파라미터에 해당하는 페이지를 정적으로 생성
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -9,6 +15,10 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`
   );
   if (!response.ok) {
+    if (response.status === 404) {
+      // 접근할 수 없는 페이지인 경우 404페이지로 이동
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
   const book = await response.json();
