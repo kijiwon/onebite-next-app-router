8feddea158a323d375457e62ae545be9cf61cf12
kijiwon <kiji1313@gmail.com>
2024-10-03 16:02:15 +0900

    deleteReviewActoion구현

--- src/components/review-item.tsx
+++ src/components/review-item.tsx
@@ -15,7 +15,7 @@ export default function ReviewItem({
       <div className={style.content}>{content}</div>
       <div className={style.bottom_container}>
         <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
-        <ReviewItemDeleteButton />
+        <ReviewItemDeleteButton reviewId={Number(id)} bookId={bookId} />
       </div>
     </div>
   );
