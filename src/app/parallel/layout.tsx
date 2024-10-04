751eb7dd7f5c4f449d77a79bbad63c62fdf78041
kijiwon <kiji1313@gmail.com>
2024-10-04 15:23:30 +0900

    parallel route slot 아래에 페이지 추가 + default페이지

--- src/app/parallel/layout.tsx
+++ src/app/parallel/layout.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+export default function Layout({
+  children,
+  sidebar,
+  feed,
+}: {
+  children: ReactNode;
+  sidebar: ReactNode;
+  feed: ReactNode;
+}) {
+  return (
+    <div>
+      <div>
+        <Link href={"/parallel"}>parallel</Link>
+        &nbsp;
+        <Link href={"/parallel/setting"}>parallel/setting</Link>
+      </div>
+      <br />
+      {/* 병렬로 렌더링 */}
+      {sidebar}
+      {feed}
+      {children}
+    </div>
+  );
 }
