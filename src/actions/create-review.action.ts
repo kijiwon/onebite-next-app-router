6b76f7d001451c7b16834e07a228a43b493aacb7
kijiwon <kiji1313@gmail.com>
2024-10-06 00:47:12 +0900

    delay 함수 제거

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,7 +1,6 @@
 "use server";
 
-import { delay } from "@/util/delay";
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 // server action을 할 함수
 export async function createReviewAction(_: any, formData: FormData) {
@@ -18,7 +17,6 @@ export async function createReviewAction(_: any, formData: FormData) {
   }
 
   try {
-    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
